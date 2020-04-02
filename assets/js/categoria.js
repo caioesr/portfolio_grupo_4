039d7fe0a10034fcd0945e5165976d56d6c3b54e
caioesr <caioesr@al.insper.edu.br>
2020-04-01 23:28:38 -0300

    começando categoria.js

--- assets/js/categoria.js
+++ assets/js/categoria.js
@@ -1,9 +1,13 @@
 document.addEventListener('DOMContentLoaded', function() {
+  let params = coDesExtract()
+  let value = params['key']
+
   let db = coDesConnect('https://projeto-codes-2020-1.firebaseio.com/')
 
   db.download('/', function(data) {
     context = data
     coDesReplace('.nav-buttons', context)
-    coDesReplace('.categorias',context)
+    context = data['portfolio'][value]
+    coDesReplace('.container-1', context)
   })
 })
